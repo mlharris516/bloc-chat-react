925e2823ccbd3257631636c45f965b700bfa71af
Meredith Harris <mlharris516@gmail.com>
2019-03-28 16:40:28 -0600

    first attempt at list chat rooms assignment

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import * as firebase from 'firebase';
+import RoomList from './Components/RoomList';
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyC8Hf9uRFuSzIVlWysyJIGPVw1ZNSQpEPM",
+    authDomain: "bloc-chat-mlh.firebaseapp.com",
+    databaseURL: "https://bloc-chat-mlh.firebaseio.com",
+    projectId: "bloc-chat-mlh",
+    storageBucket: "bloc-chat-mlh.appspot.com",
+    messagingSenderId: "349967670370"
+  };
+  firebase.initializeApp(config);
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+        <header>
+          <h1>Bloc Chat</h1>
         </header>
+        <main>
+          <RoomList firebase={firebase} />
+        </main>
       </div>
     );
   }
